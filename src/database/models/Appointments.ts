bfc497ebeaa7641df2993ed306bc8c19b03b623a
Lucas Maceda <lucasmacedamiranda@gmail.com>
2021-03-11 12:56:36 -0300

    arquivos da aula

--- src/database/models/Appointments.ts
+++ src/database/models/Appointments.ts
@@ -1,15 +1,15 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('appointments')
-class Appointments{
+class Appointments {
   @PrimaryGeneratedColumn('uuid')
-  id:       string;
+  id: string;
 
   @Column()
   provider: string;
 
   @Column('timestamp with time zone')
-  date:     Date;
+  date: Date;
 }
 
 export default Appointments;
